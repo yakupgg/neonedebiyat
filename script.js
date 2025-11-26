637cd3b4bdb8b47b0aa17cadff5604617195c8f9
yakupgg <162623019+yakupgg@users.noreply.github.com>
2025-11-26 20:36:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function checkQuiz(){let score=0;
+let q1=document.querySelector('input[name="q1"]:checked');
+let q2=document.querySelector('input[name="q2"]:checked');
+let q3=document.querySelector('input[name="q3"]:checked');
+if(q1&&q1.value==="doğru")score++;
+if(q2&&q2.value==="doğru")score++;
+if(q3&&q3.value==="doğru")score++;
+document.getElementById("result").innerHTML="Toplam Doğru: "+score+"/3";}
